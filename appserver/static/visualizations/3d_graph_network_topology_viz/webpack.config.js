00286d495d8272761d30eebbab2a0c3bc16d32c6
Erica Pescio <edro15@users.noreply.github.com>
2023-12-04 15:54:43 +0100

    Webpack: modified mode and fixed warning

--- appserver/static/visualizations/3d_graph_network_topology_viz/webpack.config.js
+++ appserver/static/visualizations/3d_graph_network_topology_viz/webpack.config.js
@@ -2,10 +2,20 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    devtool: "eval-cheap-source-map",
-    mode: "development",
+    // devtool: "eval-cheap-source-map",
+    // mode: "development",
+    mode: "production",
     entry: {
         filename: path.join(__dirname, 'src') + '/visualization_source.js'
+    },
+    resolve: {
+        // To avoid [WARNING: Multiple instances of Three.js being imported.]
+        alias: {
+            three: path.resolve(__dirname, 'node_modules/three')
+        },
+    },
+    performance: {
+        hints: false
     },
     output: {
         path: path.resolve(__dirname),
