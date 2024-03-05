6675b2ede8235287d2389885e2314afc349c9c2f
Erica Pescio <edro15@users.noreply.github.com>
2024-03-05 14:43:20 +0100

    More fixes

--- test/cypress/support/e2e.js
+++ test/cypress/support/e2e.js
@@ -18,3 +18,12 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'window.locale_name is not a function'
+    // and don't want to fail the test so we return false
+    if (err.message.includes('window.locale_name is not a function')) {
+        return false;
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+})
