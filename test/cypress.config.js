a118a31f8a81116b7d733a65b43fda8c923008e8
Erica Pescio <edro15@users.noreply.github.com>
2024-04-15 22:59:08 +0200

    Added 3 retries at failures

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -16,6 +16,7 @@ module.exports = defineConfig({
     },
     baseUrl: 'http://localhost:8000',
     defaultCommandTimeout: 40000,
+    retries: 3,
     // chromeWebSecurity: false,
     pageLoadTimeout: 600000,
     // viewportWidth: 1000,
