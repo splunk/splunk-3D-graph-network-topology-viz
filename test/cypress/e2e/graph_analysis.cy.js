d2c496c2b58f0dca9f165bbea4eb8672219ad44b
Erica Pescio <edro15@users.noreply.github.com>
2024-04-15 22:59:19 +0200

    Fixed graph_analysis tests

--- test/cypress/e2e/graph_analysis.cy.js
+++ test/cypress/e2e/graph_analysis.cy.js
@@ -34,33 +34,33 @@ describe('Graph Analysis', {
             cy.wait('@hasPageLoaded').then((interception) => {
                 /* eslint-disable no-unused-expressions */
                 expect(interception.response.body).not.to.be.empty;
+
+                // Buttons
+                cy.get('#cidds').should('exist').and('be.visible').and('not.be.disabled');
+                cy.get('#bitcoin').should('exist').and('be.visible').and('not.be.disabled');
+                cy.get('#internal').should('exist').and('be.visible').and('not.be.disabled');
+
+                // Search bar
+                cy.get('#mysearchbar1').should('exist').and('be.visible');
             });
-
-            // Buttons
-            cy.get('#cidds', { timeout: 10000 }).should('exist').and('be.visible').and('not.be.disabled');
-            cy.get('#bitcoin', { timeout: 10000 }).should('exist').and('be.visible').and('not.be.disabled');
-            cy.get('#internal', { timeout: 10000 }).should('exist').and('be.visible').and('not.be.disabled');
-
-            // Search bar
-            cy.get('#mysearchbar1', { timeout: 10000 }).should('exist').and('be.visible');
         });
 
         it("internal log data sources render properly", () => {
             cy.wait('@hasPageLoaded').then((interception) => {
                 /* eslint-disable no-unused-expressions */
                 expect(interception.response.body).not.to.be.empty;
-            });
 
-            cy.get('#internal', { timeout: 10000 }).click();
-            // Table with search results shown
-            cy.get('.splunk-table', { timeout: 10000 }).should('exist');
-            // Dropdown buttons to select fields shown
-            cy.get('.splunk-dropdown', { timeout: 10000 }).should('have.length', 3);
-            // Waiting for search to be done and dropdowns under "Select Fields" section to be shown
-            // Otherwise logout will be triggered and pending searches will throw errors
-            // cy.get("button[data-test='select']", { timeout: 10000 }).should('have.length', 3);
-            cy.get("div#statistics.viz-controller table", { timeout: 10000 }).eq(0).find('thead').should('be.visible');
-            cy.get("div#statistics.viz-controller table", { timeout: 10000 }).eq(0).find('thead').should('exist');
+                cy.get('#internal').click();
+                // Table with search results shown
+                cy.get('.splunk-table').should('exist');
+                // Dropdown buttons to select fields shown
+                cy.get('.splunk-dropdown').should('have.length', 3);
+                // Waiting for search to be done and dropdowns under "Select Fields" section to be shown
+                // Otherwise logout will be triggered and pending searches will throw errors
+                // cy.get("button[data-test='select']", { timeout: 10000 }).should('have.length', 3);
+                cy.get("div#statistics.viz-controller table").eq(0).find('thead').should('be.visible');
+                cy.get("div#statistics.viz-controller table").eq(0).find('thead').should('exist');
+            });
         });
         // TODO extend tests
     });
